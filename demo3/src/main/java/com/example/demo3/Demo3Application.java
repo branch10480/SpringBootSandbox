525c6caba6456fcf8039b34d1faade8a5bd3b3b8
branch10480 <toshiharu.imaeda@gmail.com>
2023-05-07 01:06:43 +0900

    Update

--- demo3/src/main/java/com/example/demo3/Demo3Application.java
+++ demo3/src/main/java/com/example/demo3/Demo3Application.java
@@ -1,6 +1,5 @@
 package com.example.demo3;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
